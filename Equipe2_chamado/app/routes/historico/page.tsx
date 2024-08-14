8b13d075836f33c6165274893fa1ed515fbba730
Katypires <katianepiresrc@gmail.com>
2024-08-13 21:44:08 -0400

    Tabela realizada

---
+++ Equipe2_chamado/app/routes/historico/page.tsx
@@ -0,0 +1,29 @@
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Payment[]> {
+  return [
+    {
+      id: "728ed52f",
+      acao: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      acao: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
